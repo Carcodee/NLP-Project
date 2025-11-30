8e6be735deef62418014b17995e30507074579a2
Carlos Miguel Matecki-Savino <cmatecki@my.centennialcollege.ca>
2025-11-30 14:01:37 -0500

    Fixed small printing issues

--- main.py
+++ main.py
@@ -24,11 +24,15 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 vectorizer = CountVectorizer()
+# hello, how are you, you , you?
+# [hello , how, are, you]
+# [1, 1, 1, 3]
+
 
 vectorized_matrix = vectorizer.fit_transform(df['CONTENT'])
 
 #it makes a sparse matrix where it contains the frecuency of each word on every document (1 doc 1 row- 1 row have all words possible)
-print("TFIDF shape:",vectorized_matrix.shape)
+print("frecuency table shape:",vectorized_matrix.shape)
 non_zero_cols = vectorized_matrix[0].indices
 non_zero_cols_counts = vectorized_matrix[0].data;
 non_zero_data = [(int(c), int(v)) for c, v in zip(non_zero_cols, non_zero_cols_counts)];
@@ -37,14 +41,21 @@ print("Non zero (idx, counts):", non_zero_data)
 print(vectorizer.get_feature_names_out())
 
 
+#[hello, huh, out, !, xxx, t h i s]
+#[0.8, 0.1, 0, 0, 0, 0.1]
+#[3, 2, 0, 0, 0, 1]
+
 print('---------------------------------------------')
 #it makes a sparse matrix where it stores all the tf-idf of each word 
 vectorizer_tfidf = TfidfTransformer()
 X = vectorizer_tfidf.fit_transform(vectorized_matrix)
 y = df['CLASS'].values
 
-print("TFIDF shape:", X.shape)
-print("Example features:", vectorizer_tfidf.get_feature_names_out()[:10])
+print("TFIDF table shape:",vectorized_matrix.shape)
+non_zero_cols = X[0].indices
+non_zero_cols_counts = X[0].data;
+non_zero_data = [(int(c), int(v)) for c, v in zip(non_zero_cols, non_zero_cols_counts)];
+print("Non zero (idx, counts):", non_zero_data)
 
 
 #features
@@ -52,9 +63,11 @@ print(vectorizer_tfidf.get_feature_names_out())
 
 #creates an array with all row indices from 0-350 in this case
 indices = np.arange(X.shape[0])
+#[0, 1, 2, 3, ... 350]
 rng = np.random.default_rng(seed=15)
 #randomize all the indices so if before was [0, 1, ..., 350] now is [3, 25, 8, ..., some index bewtween 0 and 350] so it becomes a random array of indices
 rng.shuffle(indices)
+#[3, 42, 4, 8, .... 39]
 
 #we create an array with remapped indices so now all rows are shuffled
 X_shuffled = X[indices]
@@ -74,7 +87,7 @@ model = MultinomialNB().fit(X=X_train, y=y_train)
 y_pred = model.predict(X=X_test)
 
 #some trivial predictions
-for truth, pred in zip(y_test[:10], y_pred[:10]):
+for truth, pred in zip(y_test[80:100], y_pred[80:100]):
     print(f"truth: {truth}, predicted: {pred}")
 
 #do here the rest of validations n fold, cross val, accuracy, etc
